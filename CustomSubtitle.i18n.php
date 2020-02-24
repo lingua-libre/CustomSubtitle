2da7b48bd7e8f46e2520b3806e0a9ece84212127
Antoine Lamielle <gh@0x010c.fr>
2020-02-24 12:30:19 +0100

    Implement a basic working version for this extension

---
+++ CustomSubtitle.i18n.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @license GPL-2.0-or-later
+ * @author Antoine Lamielle (0x010C)
+ */
+
+$magicWords = [];
+
+/* English */
+$magicWords['en'] = [
+   'subtitle' => [ 0, 'subtitle' ],
+];
